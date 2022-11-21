5f145507e51cb923cf2c3b58870fda5fd6fb4321
Jayesh SIngh <95732322+JAYESH262002@users.noreply.github.com>
2022-11-22 00:06:17 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,13 @@
+package LOVE BABBAR DSA;
+public class Bitwise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println("A & B : "+ (A&B));
+        System.out.println("A | B : "+ (A|B));
+        System.out.println("  ~A  : "+ (~A ));
+        System.out.println("A ^ B : "+ (A^B));
+
+    }
+}
